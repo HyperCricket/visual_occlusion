b61de35354f75cdc017f7b3cdbd87708185b5f75
Kevin N <kevinnguyen4221@gmail.com>
2025-10-13 15:57:21 -0500

    fixed segfault in main.py and rendering issue in test.py

--- main.py
+++ main.py
@@ -12,11 +12,29 @@ env = suite.make(
     has_offscreen_renderer=False,
     use_camera_obs=False,
 )
-
+  
 # reset the environment
 env.reset()
-
+  
 for i in range(1000):
     action = np.random.randn(*env.action_spec[0].shape) * 0.1
     obs, reward, done, info = env.step(action)  # take action in the environment
     env.render()  # render on display
+
+# def get_policy_action(obs):
+    # # random action
+    # low, high = env.action_spec
+    # return np.random.uniform(low, high)
+# 
+# obs = env.reset()
+# 
+# done = False
+# ret = 0.
+# while not done:
+    # action = get_policy_action(obs)
+    # obs, reward, done, _ = env.step(action) # play action
+    # ret += reward
+
+env.close()
+# print("rollout completed with return {}".format(ret))
+print("Success!")
